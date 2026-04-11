9a71cddb6a4408977b04e78b87f586f81a91c2fa
YushMC <mujushaxb10@gmail.com>
2026-04-11 15:05:34 -0600

    remove dist-electron files

--- dist-electron/utils/querys.js
+++
@@ -1,8 +0,0 @@
-export const prepareQuery = (query, db) => db.prepare(query);
-export const returnObjetToResponseApi = (success, message, data) => {
-    return {
-        success,
-        message,
-        data,
-    };
-};
